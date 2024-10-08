697d10a7ac457ae63bca1043118bca654d858e9a
Guilbault2063074 <81373503+Guilbault2063074@users.noreply.github.com>
2024-10-07 23:41:50 -0400

    Reorganisation

---
+++ scripts/GameManager.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+
+[GlobalClass]
+public partial class GameManager : SceneTree
+{
+	private static GameManager instance;
+	
+	private LevelManager levelManager;
+	private SaveManager saveManager;
+	
+	public static GameManager Get()
+	{
+			if (instance == null)
+			{
+				instance = new GameManager();
+				GD.Print("GameManager instance created");
+				instance._Initialize();
+			}
+			GD.Print("return gamemanager");
+			return instance;
+	}
+		
+	public override void _Initialize()
+	{
+		GD.Print("GameManager Initialized:");
+		GD.Print($"  Starting Time: {_timeElapsed}");
+	}
+		
+	public override bool _Process(double delta)
+	{
+		_timeElapsed += delta;
+		
+		if (Input.IsActionPressed("ui_cancel")) //
+		{
+			GD.Print("Escape pressed, handle exit logic");
+		}
+		
+		return true;
+	}
+		
+	
+}
